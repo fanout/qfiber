f59afe62fe11646a6fb1d1ecbc761285f9605e14
Justin Karneges <justin@affinix.com>
2015-05-22 15:22:04 -0700

    configure script and .gitignore

--- examples/webserver/webserver.cpp
+++ examples/webserver/webserver.cpp
@@ -14,40 +14,71 @@ void initMeta()
 void handler(const QVariantList &args)
 {
 	int fd = args[0].toInt();
+	QFiber::Channel c = qvariant_cast<QFiber::Channel>(args[1]);
 
+	bool quit = false;
 	QByteArray buf(16384, 0);
 	while(true)
 	{
 		ssize_t ret = lthread_recv(fd, buf.data(), buf.size(), 0, 0);
 		if(ret <= 0)
 			return;
+		if(buf.contains("/quit"))
+			quit = true;
 		if(buf.contains("\r\n\r\n"))
 			break;
 	}
 
-	QByteArray response = "HTTP/1.0 200 OK\r\nContent-Length: 12\r\n\r\nHello World\n";
-	lthread_send(fd, response.data(), response.size(), 0);
-	lthread_close(fd);
+	if(quit)
+	{
+		QByteArray response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 9\r\n\r\nQuitting\n";
+		lthread_send(fd, response.data(), response.size(), 0);
+		lthread_close(fd);
+		c.write("quit");
+	}
+	else
+	{
+		QByteArray response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello World\n";
+		lthread_send(fd, response.data(), response.size(), 0);
+		lthread_close(fd);
+	}
 }
 
 void receiver(const QVariantList &args)
 {
 	int id = args[0].toInt();
-	QFiber::Channel c = qvariant_cast<QFiber::Channel>(args[1]);
+	QFiber::Channel lc = qvariant_cast<QFiber::Channel>(args[1]);
+	QFiber::Channel lwc = qvariant_cast<QFiber::Channel>(args[2]);
 
 	printf("%d: receiver started (thread=%p)\n", id, QThread::currentThread());
+	QFiber::Channel hc;
+
+	QFiber::Poller poller;
+	poller.addRead(lc);
+	poller.addRead(hc);
 
 	while(true)
 	{
-		QVariantList params = c.read();
-		int fd = params[0].toInt();
-		QHostAddress addr = qvariant_cast<QHostAddress>(params[1]);
+		QFiber::Channel c = poller.poll();
 
-		// spawn a handler to do the work
-		printf("%d: incoming connection from %s\n", id, qPrintable(addr.toString()));
-		QFiber::Fiber handlerFiber("handler");
-		handlerFiber.setDetached(true);
-		handlerFiber.start(handler, fd);
+		if(c == lc)
+		{
+			QVariantList params = lc.read();
+			int fd = params[0].toInt();
+			QHostAddress addr = qvariant_cast<QHostAddress>(params[1]);
+
+			// spawn a handler to do the work
+			printf("%d: incoming connection from %s\n", id, qPrintable(addr.toString()));
+			QFiber::Fiber handlerFiber("handler");
+			handlerFiber.setDetached(true);
+			handlerFiber.start(handler, fd, QVariant::fromValue(hc));
+		}
+		else // hc
+		{
+			QString command = hc.readSingle().toString();
+			if(command == "quit")
+				break;
+		}
 	}
 }
 
@@ -77,6 +108,9 @@ void listener(const QVariantList &args)
 		receiverFiber.setThreaded(true);
 		receiverFiber.start(receiver, n, QVariant::fromValue(c));
 	}
+
+	QFiber::Poller poller;
+	poller.addRead(server);
 
 	int r = 0;
 	while(true)
